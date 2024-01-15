ff04e45b05136ef06bd4a90fe6f747df042e1bcf
MarkTanashchuk <mark.tanashchuk@gmail.com>
2024-01-15 04:55:39 +0200

    feat: add home page with a dropdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+  import Weather from '$widgets/weather/index.svelte';
+  import { Content, Dropdown } from 'carbon-components-svelte';
+
+  const items = [{ id: '0', text: 'Ukraine' }];
+
+  let cityId: string = '0';
 </script>
 
-<div class="w-full h-full flex flex-col items-center">
-  <h1>Welcome to your library project</h1>
-  <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-</div>
+<Content class="flex flex-col items-center justify-center w-1/2 m-auto">
+  <h1 class="text-4xl font-bold text-gray-800">Weather App</h1>
+
+  <div class="flex flex-col w-full max-w-md gap-8">
+    <Dropdown titleText="Choose city" bind:selectedId={cityId} {items} />
+
+    <Weather kind="sunny" temperature={20} title={'New York'} />
+  </div>
+</Content>
