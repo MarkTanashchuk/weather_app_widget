da65cb1b6c332a4cb0806d4e59986f69a4ebe19e
MarkTanashchuk <mark.tanashchuk@gmail.com>
2024-01-17 06:53:13 +0200

    refactor(design): improve weather card

--- src/widgets/weather/ui/card.svelte
+++ src/widgets/weather/ui/card.svelte
@@ -10,13 +10,16 @@
 
 <Tile class="flex items-center justify-between rounded-lg w-full px-0">
   {#if mode === 'normal'}
-    <div class="flex flex-col gap-2">
-      <h2 class="tracking-tight text-3xl font-bold text-gray-800">{title}</h2>
-      <h4 class="text-2xl font-bold text-gray-800">{temperature}°C</h4>
-      <p class="text-gray-600 capitalize">{kind}</p>
+    <div class="flex flex-wrap gap-8">
+      <h2 class="text-5xl font-bold text-gray-800">{temperature}°C</h2>
+      <div class="flex items-center">
+        <div>
+          <h3 class="tracking-tight text-3xl font-bold text-gray-800">{title}</h3>
+          <p class="text-gray-600 capitalize">{kind}</p>
+        </div>
+        <img class="w-16 h-16" src={iconURL} alt={kind} />
+      </div>
     </div>
-
-    <img class="w-16 h-16" src={iconURL} alt={kind} />
   {:else if mode === 'compact'}
     <div class="flex justify-between items-center w-full text-gray-600">
       <div class="tracking-tight font-bold">{title}</div>
