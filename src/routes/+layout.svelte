6cf96aa34ce67f55782d553bb0e37cf10ff90898
MarkTanashchuk <mark.tanashchuk@gmail.com>
2024-01-17 06:52:28 +0200

    refactor(design): remove unnecessary wrapper

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script>
-  import { Content } from 'carbon-components-svelte';
-
   import 'carbon-components-svelte/css/g10.css';
   import '../app.css';
 </script>
 
-<Content>
-  <slot />
-</Content>
+<slot />
