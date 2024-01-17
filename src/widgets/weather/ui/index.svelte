1436d52003a632759a15d5c31d6fbba54591c299
MarkTanashchuk <mark.tanashchuk@gmail.com>
2024-01-17 07:01:46 +0200

    feat: update weather widget
    feat: store search history
    
    refactor: get rid of #await in markup and place it in logic
    
    refactor(design): improve widget design by making use of height

--- src/widgets/weather/ui/index.svelte
+++ src/widgets/weather/ui/index.svelte
@@ -1,64 +1,92 @@
 <script context="module" lang="ts">
+  import type { SearchEvent } from '$features/search/ui.svelte';
   import type { WeatherData } from '$widgets/weather/index.js';
   import type { DropdownItem } from 'carbon-components-svelte/src/Dropdown/Dropdown.svelte';
+  import type { Writable } from 'svelte/store';
 </script>
 
 <script lang="ts">
   import { Search } from '$features/search/index.js';
-  import { getWeather, WeatherCard, WeatherTable } from '$widgets/weather/index.js';
-  import { Tile } from 'carbon-components-svelte';
-
-  import type { SearchEvent } from '$features/search/ui.svelte';
   import { persistentStore } from '$shared/utils.js';
-  import type { Writable } from 'svelte/store';
+  import { getWeather, WeatherCard, WeatherTable } from '$widgets/weather/index.js';
+  import { Button, Tile } from 'carbon-components-svelte';
+  import DownloadIcon from 'carbon-icons-svelte/lib/Download.svelte';
+  import FilterIcon from 'carbon-icons-svelte/lib/Filter.svelte';
+  import { nanoid } from 'nanoid';
 
   export let showHistory: boolean = false;
+
+  let weatherRequestsPending = 0;
 
   const searchHistory: Writable<DropdownItem[]> = persistentStore('searchHistory', []);
   const weatherHistory: Writable<WeatherData[]> = persistentStore('weatherHistory', []);
 
-  let currentCityWeather: Promise<WeatherData> | null = null;
+  async function handleSearch(event: SearchEvent) {
+    weatherRequestsPending += 1;
 
-  function handleSearch(event: SearchEvent) {
-    currentCityWeather = getWeather(event.detail.value);
+    try {
+      let weatherData = await getWeather(event.detail.value);
+
+      $weatherHistory = [weatherData, ...$weatherHistory];
+      $searchHistory = [{ id: nanoid(), text: event.detail.value }, ...$searchHistory];
+      weatherRequestsPending -= 1;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  function handleStoringCurrentWeatherREFACTOR(node: HTMLDivElement, data: WeatherData) {
-    $weatherHistory = [data, ...$weatherHistory];
+  function handleClearHistory() {
+    $weatherHistory = [];
+    weatherRequestsPending = 0;
   }
+
+  let selectedWeather: WeatherData | undefined;
+  $: selectedWeather = $weatherHistory[0];
 </script>
 
-<div class="flex flex-col w-full max-w-md gap-8 pt-8">
+<div class="flex flex-col w-full h-full gap-8 pt-8">
   <Search on:search={handleSearch} searchHistory={$searchHistory} />
 
-  <Tile>
-    <h2 class="font-bold">Current weather:</h2>
-    <div class="py-2">
-      {#if currentCityWeather !== null}
-        {#await currentCityWeather}
-          <p>Weather is loading...</p>
-        {:then data}
-          <div class="contents" use:handleStoringCurrentWeatherREFACTOR={data}>
-            <WeatherCard
-              kind={data.description}
-              iconURL={data.iconURL}
-              temperature={data.main.temp}
-              title={data.name}
-            />
-          </div>
-        {:catch error}
-          <p class="text-red-600">{error.message}</p>
-        {/await}
+  <div class="flex justify-between w-full h-full gap-8">
+    <Tile class="flex flex-col flex-1 justify-between">
+      {#if selectedWeather}
+        <h2 class="font-bold">Latest search:</h2>
+        <div class="py-2">
+          <WeatherCard
+            kind={selectedWeather.description}
+            iconURL={selectedWeather.iconURL}
+            temperature={selectedWeather.main.temp}
+            title={selectedWeather.name}
+          />
+
+          {#if weatherRequestsPending > 0}
+            <div class="flex gap-4 items-center justify-between">
+              <p>Weather request is loading...</p>
+              <DownloadIcon />
+            </div>
+          {/if}
+        </div>
       {:else}
         <p class="text-black">
           Enter the city name and click on the search button to obtain the current weather for the
           desired city
         </p>
       {/if}
-    </div>
-  </Tile>
+    </Tile>
 
-  {#if showHistory}
-    <WeatherTable items={$weatherHistory} />
-  {/if}
+    {#if showHistory}
+      <div class="flex flex-col h-full gap-4">
+        <WeatherTable items={$weatherHistory} />
+        <Button
+          kind="danger"
+          class="w-full max-w-none bg-[#da1e28]"
+          icon={FilterIcon}
+          iconDescription="Clear History"
+          on:click={handleClearHistory}
+        >
+          Clear History
+        </Button>
+      </div>
+    {/if}
+  </div>
 </div>
